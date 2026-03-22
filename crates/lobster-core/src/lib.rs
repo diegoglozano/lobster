598f86ec2ae81bd5ff8e5bafef17acdc30dafbff
diegoglozano <dgl_1096@hotmail.com>
2026-03-22 21:30:09 +0100

    [feat] first commit

---
+++ crates/lobster-core/src/lib.rs
@@ -0,0 +1,244 @@
+use chrono::{DateTime, Utc};
+use rust_decimal::prelude::Decimal;
+use std::collections::{BTreeMap, VecDeque};
+use uuid::Uuid;
+
+type Price = Decimal;
+
+pub enum OrderSide {
+    Ask,
+    Bid,
+}
+
+pub enum OrderType {
+    Limit,
+    Market,
+}
+
+pub struct Order {
+    id: Uuid,
+    timestamp: DateTime<Utc>,
+    side: OrderSide,
+    order_type: OrderType,
+    units: u64,
+    price: Price,
+}
+
+impl Order {
+    pub fn new(side: OrderSide, order_type: OrderType, units: u64, price: Price) -> Self {
+        Order {
+            id: Uuid::new_v4(),
+            timestamp: Utc::now(),
+            side,
+            order_type,
+            units,
+            price,
+        }
+    }
+}
+
+pub struct OrderBook {
+    bids: BTreeMap<Price, VecDeque<Order>>,
+    asks: BTreeMap<Price, VecDeque<Order>>,
+}
+
+impl OrderBook {
+    pub fn new() -> Self {
+        OrderBook {
+            bids: BTreeMap::new(),
+            asks: BTreeMap::new(),
+        }
+    }
+
+    pub fn units_at_price(&self, side: OrderSide, price: Price) -> Option<u64> {
+        match side {
+            OrderSide::Ask => {
+                let Some(ask_queue) = self.asks.get(&price) else {
+                    return None;
+                };
+                let Some(units) = ask_queue.front() else {
+                    return None;
+                };
+                return Some(units.units);
+            }
+            OrderSide::Bid => {
+                let Some(bid_queue) = self.bids.get(&price) else {
+                    return None;
+                };
+                let Some(units) = bid_queue.front() else {
+                    return None;
+                };
+                return Some(units.units);
+            }
+        }
+    }
+
+    pub fn add_order(&mut self, order: Order) {
+        match order.side {
+            OrderSide::Ask => {
+                self.asks.entry(order.price).or_default().push_back(order);
+            }
+            OrderSide::Bid => {
+                self.bids.entry(order.price).or_default().push_back(order);
+            }
+        }
+    }
+
+    pub fn match_orders(&mut self) -> Option<(Uuid, Uuid)> {
+        if self.bids.is_empty() || self.asks.is_empty() {
+            return None;
+        }
+        let Some((bid_price, _)) = self.bids.last_key_value() else {
+            return None;
+        };
+        let Some((ask_price, _)) = self.asks.first_key_value() else {
+            return None;
+        };
+
+        let bid_price = *bid_price;
+        let ask_price = *ask_price;
+
+        if bid_price < ask_price {
+            return None;
+        } else {
+            let Some(bid_queue) = self.bids.get_mut(&bid_price) else {
+                return None;
+            };
+            let Some(ask_queue) = self.asks.get_mut(&ask_price) else {
+                return None;
+            };
+            let Some(bid_order) = bid_queue.front_mut() else {
+                return None;
+            };
+            let Some(ask_order) = ask_queue.front_mut() else {
+                return None;
+            };
+            let bid_id = bid_order.id;
+            let bid_units = bid_order.units;
+            let ask_id = ask_order.id;
+            let ask_units = ask_order.units;
+
+            if bid_units < ask_units {
+                bid_queue.pop_front();
+                if bid_queue.is_empty() {
+                    self.bids.remove_entry(&bid_price);
+                }
+                ask_order.units = ask_order.units - bid_units;
+            } else if bid_units > ask_units {
+                ask_queue.pop_front();
+                if ask_queue.is_empty() {
+                    self.asks.remove_entry(&ask_price);
+                }
+                bid_order.units = bid_order.units - ask_units;
+            } else {
+                ask_queue.pop_front();
+                if ask_queue.is_empty() {
+                    self.asks.remove_entry(&ask_price);
+                }
+                bid_queue.pop_front();
+                if bid_queue.is_empty() {
+                    self.bids.remove_entry(&bid_price);
+                }
+            };
+            return Some((bid_id, ask_id));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_order_has_correct_units() {
+        let order = Order::new(OrderSide::Ask, OrderType::Limit, 50, Decimal::new(50, 0));
+        assert_eq!(order.units, 50);
+    }
+
+    #[test]
+    fn new_order_has_correct_price() {
+        let order = Order::new(OrderSide::Ask, OrderType::Limit, 50, Decimal::new(50, 0));
+        assert_eq!(order.price, Decimal::new(50, 0))
+    }
+
+    #[test]
+    fn no_match_in_book() {
+        let mut order_book = OrderBook::new();
+        let ask = Order::new(OrderSide::Ask, OrderType::Limit, 50, Decimal::new(51, 0));
+        let bid = Order::new(OrderSide::Bid, OrderType::Limit, 50, Decimal::new(50, 0));
+        order_book.add_order(ask);
+        order_book.add_order(bid);
+
+        let is_match = order_book.match_orders();
+        assert_eq!(is_match, None);
+    }
+
+    #[test]
+    fn exact_match_in_book() {
+        let mut order_book = OrderBook::new();
+        let ask = Order::new(OrderSide::Ask, OrderType::Limit, 50, Decimal::new(50, 0));
+        let bid = Order::new(OrderSide::Bid, OrderType::Limit, 50, Decimal::new(50, 0));
+        let ask_id = ask.id;
+        let bid_id = bid.id;
+
+        order_book.add_order(ask);
+        order_book.add_order(bid);
+
+        let is_match = order_book.match_orders();
+        assert_eq!(is_match, Some((bid_id, ask_id)));
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Bid, Decimal::new(50, 0)),
+            None,
+        );
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Ask, Decimal::new(50, 0)),
+            None,
+        );
+    }
+
+    #[test]
+    fn partial_match_in_book_bid_smaller() {
+        let mut order_book = OrderBook::new();
+        let ask = Order::new(OrderSide::Ask, OrderType::Limit, 50, Decimal::new(50, 0));
+        let bid = Order::new(OrderSide::Bid, OrderType::Limit, 49, Decimal::new(50, 0));
+        let ask_id = ask.id;
+        let bid_id = bid.id;
+
+        order_book.add_order(ask);
+        order_book.add_order(bid);
+
+        let is_match = order_book.match_orders();
+        assert_eq!(is_match, Some((bid_id, ask_id)));
+
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Bid, Decimal::new(50, 0)),
+            None
+        );
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Ask, Decimal::new(50, 0)),
+            Some(1)
+        );
+    }
+    #[test]
+    fn partial_match_in_book_ask_smaller() {
+        let mut order_book = OrderBook::new();
+        let ask = Order::new(OrderSide::Ask, OrderType::Limit, 49, Decimal::new(50, 0));
+        let bid = Order::new(OrderSide::Bid, OrderType::Limit, 50, Decimal::new(50, 0));
+        let ask_id = ask.id;
+        let bid_id = bid.id;
+
+        order_book.add_order(ask);
+        order_book.add_order(bid);
+
+        let is_match = order_book.match_orders();
+        assert_eq!(is_match, Some((bid_id, ask_id)));
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Bid, Decimal::new(50, 0)),
+            Some(1)
+        );
+        assert_eq!(
+            order_book.units_at_price(OrderSide::Ask, Decimal::new(50, 0)),
+            None
+        );
+    }
+}
---
+++ crates/lobster-engine/src/lib.rs
@@ -0,0 +1,81 @@
+use crossbeam::channel::{Receiver, Sender};
+use lobster_core::{Order, OrderBook, OrderSide, OrderType};
+use rust_decimal::Decimal;
+use uuid::Uuid;
+
+pub struct Message {
+    order: Order,
+    response: Sender<Option<(Uuid, Uuid)>>,
+}
+
+pub struct EngineHandle {
+    sender: Sender<Message>,
+}
+
+impl EngineHandle {
+    pub fn submit(&self, order: Order) -> Option<(Uuid, Uuid)> {
+        let (response_tx, response_rx) = crossbeam::channel::bounded(1);
+        self.sender
+            .send(Message {
+                order,
+                response: response_tx,
+            })
+            .unwrap();
+        response_rx.recv().unwrap()
+    }
+}
+
+pub struct MatchingEngine {
+    order_book: OrderBook,
+    receiver: Receiver<Message>,
+}
+
+impl MatchingEngine {
+    pub fn new() -> (Self, EngineHandle) {
+        let (tx, rx) = crossbeam::channel::unbounded();
+        let engine = MatchingEngine {
+            order_book: OrderBook::new(),
+            receiver: rx,
+        };
+        let handle = EngineHandle { sender: tx };
+        (engine, handle)
+    }
+
+    pub fn run(mut self) {
+        std::thread::spawn(move || {
+            loop {
+                match self.receiver.recv() {
+                    Ok(msg) => {
+                        self.order_book.add_order(msg.order);
+                        let is_match = self.order_book.match_orders();
+                        msg.response.send(is_match).ok();
+                    }
+                    Err(_) => break,
+                }
+            }
+        });
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn submit_orders() {
+        let (engine, handle) = MatchingEngine::new();
+        engine.run();
+        handle.submit(Order::new(
+            OrderSide::Ask,
+            OrderType::Limit,
+            50,
+            Decimal::new(50, 0),
+        ));
+        handle.submit(Order::new(
+            OrderSide::Bid,
+            OrderType::Limit,
+            50,
+            Decimal::new(50, 0),
+        ));
+    }
+}
